fba9257869e804fbedaf54b8dcbe336633bff76d
Jürgen Reiser <juergen.reiser@neo-q.de>
2020-10-24 08:39:55 +0200

    Stuff

---
+++ DicomReader/Models/StudyQueryData.cs
@@ -0,0 +1,9 @@
+﻿namespace DicomReader.Models
+{
+    public class StudyQueryData
+    {
+        public string PatiendId { get; set; }
+        public string StudyInstanceUid { get; set; }
+        public string SeriesInstanceUid { get; set; }
+    }
+}
