aa8737e10fea4ddc3d09a9d2f4402c7a8cb49a67
Jürgen Reiser <juergen.reiser@neo-q.de>
2020-10-27 09:36:19 +0100

    Add pacs server configuration controls

---
+++ DicomCrawler/DicomCrawler/Controls/PacsServerConfigurationTab/PacsServerConfigurationTabPage.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using DicomCrawler.Helpers;
+using DicomCrawler.Models;
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace DicomCrawler.Controls.PacsServerConfigurationTab
+{
+    public class PacsServerConfigurationTabPage : TabPage
+    {
+        private PacsServerConfigurationTabPage()
+        {
+            Text = "PACS Configuration";
+        }
+
+        public static PacsServerConfigurationTabPage Create()
+        {
+            var modifyButton = CreateModifyButton();
+            var saveButton = CreateSaveButton();
+
+            return new PacsServerConfigurationTabPage
+            {
+                Padding = Gap.Medium,
+                DataContext = ViewModelFactory.Create<PacsServerConfiguration>(),
+                Content = new StackLayout
+                {
+                    Items =
+                    {
+                        new StackLayoutItem
+                        {
+                            Control = PacsServerConfigurationTableLayout.Create()
+                        },
+                        new StackLayoutItem
+                        {
+                            Control = new StackLayout
+                            {
+                                Padding = new Padding(0, 8, 0, 0),
+                                Orientation = Orientation.Horizontal,
+                                Items =
+                                {
+                                    new StackLayoutItem
+                                    {
+                                        Control = modifyButton
+                                    },
+                                    new StackLayoutItem
+                                    {
+                                        Control = saveButton
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        private static Button CreateModifyButton()
+        {
+            var modifyButton = new Button
+            {
+                Text = "Modify"
+            };
+            modifyButton.Click += (sender, args) => OnModify((PacsServerConfiguration) ((Button) sender).DataContext);
+            modifyButton.BindDataContext(b => b.Enabled, (PacsServerConfiguration configuration) => configuration.IsReadOnly);
+
+            return modifyButton;
+        }
+
+        private static Button CreateSaveButton()
+        {
+            var saveButton = new Button
+            {
+                Text = "Save",
+            };
+            saveButton.Click += (sender, args) => OnSave((PacsServerConfiguration) ((Button) sender).DataContext);
+            saveButton.BindDataContext(b => b.Enabled, (PacsServerConfiguration configuration) => !configuration.IsReadOnly);
+            
+            return saveButton;
+        }
+
+        private static void OnModify(PacsServerConfiguration configuration)
+        {
+            configuration.IsReadOnly = false;
+        }
+
+        private static void OnSave(PacsServerConfiguration configuration)
+        {
+            var settings = Application.Instance.GetSettings();
+
+            try
+            {
+                var host = configuration.Host;
+                var port = configuration.Port.IsNullOrEmpty() ? 0 : int.Parse(configuration.Port);
+                var callingAet = configuration.CallingAet;
+                var calledAet = configuration.CalledAet;
+
+                settings.PacsHost = host;
+                settings.PacsPort = port;
+                settings.PacsCallingAet = callingAet;
+                settings.PacsCalledAet = calledAet;
+                configuration.IsReadOnly = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Input is not valid", MessageBoxType.Error);
+            }
+        }
+    }
+}
---
+++ DicomCrawler/DicomCrawler/Controls/PacsServerConfigurationTab/PacsServerConfigurationTableLayout.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq.Expressions;
+using DicomCrawler.Models;
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace DicomCrawler.Controls.PacsServerConfigurationTab
+{
+    public class PacsServerConfigurationTableLayout : TableLayout
+    {
+        private PacsServerConfigurationTableLayout() : base(2, 4)
+        {
+        }
+
+        public static PacsServerConfigurationTableLayout Create()
+        {
+            var table = new PacsServerConfigurationTableLayout
+            {
+                Spacing = new Size(8, 4)
+            };
+
+            table.Add(CreateLabel("Host:"), 0, 0);
+            table.Add(CreateLabel("Port:"), 0, 1);
+            table.Add(CreateLabel("CallingAet:"), 0, 2);
+            table.Add(CreateLabel("CalledAet:"), 0, 3);
+
+            table.Add(CreateTextBox(_ => _.Host), 1, 0);
+            table.Add(CreateTextBox(_ => _.Port), 1, 1);
+            table.Add(CreateTextBox(_ => _.CallingAet), 1, 2);
+            table.Add(CreateTextBox(_ => _.CalledAet), 1, 3);
+
+            return table;
+        }
+
+        private static Label CreateLabel(string text)
+        {
+            return new Label
+            {
+                Text = text,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+        }
+
+        private static TextBox CreateTextBox(Expression<Func<PacsServerConfiguration, string>> propertyExpression)
+        {
+            var textBox = new TextBox();
+            textBox.BindDataContext(_ => _.ReadOnly, (PacsServerConfiguration configuration) => configuration.IsReadOnly);
+            textBox.TextBinding.BindDataContext(propertyExpression);
+
+            return textBox;
+        }
+    }
+}
