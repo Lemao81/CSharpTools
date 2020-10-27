1888c461f40b30aa156f050b32f6153ebe2d7607
Jürgen Reiser <juergen.reiser@neo-q.de>
2020-10-27 09:35:47 +0100

    Add app controls

---
+++ DicomCrawler/DicomCrawler/Controls/App/AppMenuBar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using DicomCrawler.Helpers;
+using Eto.Forms;
+
+namespace DicomCrawler.Controls.App
+{
+    public class AppMenuBar : MenuBar
+    {
+        private AppMenuBar()
+        {
+        }
+
+        public static AppMenuBar Create(Control parent, Version currentVersion)
+        {
+            return new AppMenuBar
+            {
+                Items =
+                {
+                    new ButtonMenuItem
+                    {
+                        Text = "&File", Items = { Commands.ClickMe(parent) }
+                    },
+                },
+                ApplicationItems =
+                {
+                    new ButtonMenuItem
+                    {
+                        Text = "&Preferences..."
+                    },
+                },
+                QuitItem = Commands.QuitApp(parent),
+                AboutItem = Commands.ShowAboutDialog(parent, CreateAboutDialog(currentVersion))
+            };
+        }
+
+        private static AboutDialog CreateAboutDialog(Version _currentVersion) =>
+            new AboutDialog
+            {
+                Developers = new[] { "Jürgen Reiser" },
+                Title = "Dicom Crawler",
+                ProgramName = "Dicom Crawler",
+                Version = $"Version: {_currentVersion}"
+            };
+    }
+}
---
+++ DicomCrawler/DicomCrawler/Controls/App/AppToolBar.cs
@@ -0,0 +1,19 @@
+﻿using Eto.Forms;
+
+namespace DicomCrawler.Controls.App
+{
+    public class AppToolBar : ToolBar
+    {
+        private AppToolBar()
+        {
+        }
+
+        public static AppToolBar Create()
+        {
+            return new AppToolBar
+            {
+                Items = { }
+            };
+        }
+    }
+}
