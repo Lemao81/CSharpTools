c4a8cf7686fcb0e948d0c77545d9c21abdc06a18
Jürgen Reiser <juergen.reiser@neo-q.de>
2020-10-27 09:36:37 +0100

    Add query result tab controls

---
+++ DicomCrawler/DicomCrawler/Controls/QueryResultTab/DicomQueryResultTabPage.cs
@@ -0,0 +1,17 @@
+﻿using Eto.Forms;
+
+namespace DicomCrawler.Controls.QueryResultTab
+{
+    public class DicomQueryResultTabPage : TabPage
+    {
+        private DicomQueryResultTabPage()
+        {
+            Text = "Result";
+        }
+
+        public static DicomQueryResultTabPage Create()
+        {
+            return new DicomQueryResultTabPage();
+        }
+    }
+}
