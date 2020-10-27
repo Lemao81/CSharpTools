206e32faa81495b0c27549bb63ff78273f750448
Jürgen Reiser <juergen.reiser@neo-q.de>
2020-10-27 09:38:04 +0100

    Refactor main form

--- DicomCrawler/DicomCrawler/MainForm.cs
+++ DicomCrawler/DicomCrawler/MainForm.cs
@@ -1,18 +1,55 @@
+using System;
+using DicomCrawler.Controls;
+using DicomCrawler.Controls.App;
+using DicomCrawler.Controls.DicomQueryTab;
+using DicomCrawler.Controls.PacsServerConfigurationTab;
+using DicomCrawler.Controls.QueryResultTab;
+using DicomCrawler.Helpers;
+using DicomCrawler.Models;
 using Eto.Forms;
 using Eto.Drawing;
 
 namespace DicomCrawler
 {
-    public partial class MainForm : Form
+    public class MainForm : Form
     {
+        private readonly Version _currentVersion = new Version(0, 0, 1);
+
         public MainForm()
         {
-            Title = "Dicom Crawler";
-            ClientSize = new Size(400, 350);
+            InitializeApp();
 
+            Title = "Dicom Crawler";
+            Size = new Size(600, 450);
+            Location = GetCenterLocation();
+            Padding = Gap.Small;
             Content = CreateContent();
-            Menu = CreateMenuBar();
-            ToolBar = CreateToolBar();
+            Menu = AppMenuBar.Create(this, _currentVersion);
+            // ToolBar = AppToolBar.Create();
+        }
+
+        private Point GetCenterLocation() => new Point((int) (Screen.WorkingArea.Width / 2 - Size.Width / 2f), (int) (Screen.WorkingArea.Height / 2 - Size.Height / 2f));
+
+        private static Control CreateContent() =>
+            new TabControl
+            {
+                Pages =
+                {
+                    DicomQueryTabPage.Create(),
+                    DicomQueryResultTabPage.Create(),
+                    PacsServerConfigurationTabPage.Create()
+                }
+            };
+
+        private static void InitializeApp()
+        {
+            Application.Instance.Properties.Set(Settings.SettingsKey, new Settings());
+            Application.Instance.UnhandledException += OnUnhandledException;
+        }
+
+        private static void OnUnhandledException(object sender, Eto.UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Unexpected error occurred", MessageBoxType.Error);
         }
     }
 }
