d92e360fdc372fc8ffbd94206e00b798c847e58e
Jürgen Reiser <juergen.reiser@neo-q.de>
2021-01-30 21:49:44 +0100

    Add ui render test project

---
+++ UiRenderTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using PlaywrightSharp;
+
+namespace UiRenderTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using var playwright = Playwright.CreateAsync().Result;
+            var browser = playwright.Chromium.LaunchAsync().Result;
+            var page = browser.NewPageAsync().Result;
+            page.GoToAsync("http://rrtestneo-q.vialutions.biz/Frontend/login").Wait();
+            page.ScreenshotAsync("renderoutput.html").Wait();
+        }
+    }
+}
