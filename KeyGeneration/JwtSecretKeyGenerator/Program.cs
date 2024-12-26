76e31234b627a2ac820e086a1c92d30662cc73f9
JuergenReiser <juergen.reiser@neo-q.de>
2024-12-26 11:32:14 +0100

    Remove namespace scope

--- KeyGeneration/JwtSecretKeyGenerator/Program.cs
+++ KeyGeneration/JwtSecretKeyGenerator/Program.cs
@@ -1,14 +1,12 @@
-﻿namespace JwtSecretKeyGenerator
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            var rng   = System.Security.Cryptography.RandomNumberGenerator.Create();
-            var bytes = new byte[256 / 8];
-            rng.GetBytes(bytes);
-            Console.WriteLine(Convert.ToBase64String(bytes));
-        }
-    }
+﻿namespace JwtSecretKeyGenerator;
 
+internal class Program
+{
+    private static void Main(string[] _)
+    {
+        var rng   = System.Security.Cryptography.RandomNumberGenerator.Create();
+        var bytes = new byte[256 / 8];
+        rng.GetBytes(bytes);
+        Console.WriteLine(Convert.ToBase64String(bytes));
+    }
 }
