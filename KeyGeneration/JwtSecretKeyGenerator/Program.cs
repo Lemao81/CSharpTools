80ce08479f19e3608ce79c2554c52480cfa9c45e
JuergenReiser <juergen.reiser@neo-q.de>
2024-12-26 11:28:59 +0100

    Add jwt secret generator tool

---
+++ KeyGeneration/JwtSecretKeyGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace JwtSecretKeyGenerator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rng   = System.Security.Cryptography.RandomNumberGenerator.Create();
+            var bytes = new byte[256 / 8];
+            rng.GetBytes(bytes);
+            Console.WriteLine(Convert.ToBase64String(bytes));
+        }
+    }
+
+}
